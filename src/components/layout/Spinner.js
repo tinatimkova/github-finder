0ffced32deee9eef880eb94dd48c6983d0e40ced
timosha <tinaatimkovaa@gmail.com>
2020-11-25 14:32:20 -0500

    created environment variables

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,12 +1,10 @@
 import React, {Fragment} from 'react';
 import spinner from './spinner.gif';
 
-function Spinner() {
-    return (
+const Spinner = () =>
         <Fragment>
             <img src={spinner} alt='Loading...' style={{ width: '200px', margin: 'auto', display: 'block'}}/>
         </Fragment>
-    )
-}
+
 
 export default Spinner
