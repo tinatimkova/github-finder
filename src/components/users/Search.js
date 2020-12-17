e9996a3567ab13afab23ed92d5c40d2b842c4695
timosha <tinaatimkovaa@gmail.com>
2020-12-16 20:16:30 -0500

    passing props from search component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 
 class Search extends Component {
+    state={
+        text: ''
+    }
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired
+    }
+
+    onChange = e => this.setState({ [e.target.name] : e.target.value })
+
+    onSubmit(e) {
+        e.preventDefault()
+        this.props.searchUsers(this.state.text)
+        this.setState({ text: '' })
+    }
+
     render() {
         return (
             <div>
-                <form className='form'>
-                    <input type='text' name='text' placeholder='Search Users...' />
-                    <input type='submit' value='Search' className='btn btn-dark btn-clock' />
+                <form className='form' onSubmit={this.onSubmit.bind(this)}>
+                    <input type='text' name='text' placeholder='Search Users...' autoComplete='off' value={this.state.text} onChange={this.onChange} />
+                    <input type='submit' value='Search' className='btn btn-dark btn-block' />
                 </form>  
             </div>
         )
