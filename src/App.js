ed5ebcffdd4162608209c521764480d52313e068
timosha <tinaatimkovaa@gmail.com>
2020-12-17 11:56:15 -0500

    create an alert msg

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 import Search from './components/users/Search'
+import Alert from './components/layout/Alert'
 import './App.css';
 import axios from 'axios'
 
@@ -9,7 +10,8 @@ class App extends Component {
 
   state={
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   // async componentDidMount() {
@@ -34,6 +36,13 @@ class App extends Component {
     this.setState({ users: [] })
   }
 
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type }})
+
+    setTimeout(() => this.setState({ alert: null }), 5000)
+  }
+
   render () {
 
     const { users, loading } = this.state
@@ -42,7 +51,13 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false}/>
+          <Alert alert={this.state.alert} />
+          <Search 
+            searchUsers={this.searchUsers} 
+            clearUsers={this.clearUsers} 
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
           <Users users={users} loading={loading} />
         </div>
       </div>
