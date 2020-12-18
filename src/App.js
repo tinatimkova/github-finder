976bdb9bd0859dc9db4df4a7e33d927ddd6ca806
timosha <tinaatimkovaa@gmail.com>
2020-12-18 11:24:25 -0500

    create a user component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import User from './components/users/User'
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
@@ -12,6 +13,7 @@ class App extends Component {
 
   state={
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -45,9 +47,18 @@ class App extends Component {
     setTimeout(() => this.setState({ alert: null }), 5000)
   }
 
+  // Get a single Github user
+  getUser = async (username) => {
+    this.setState({ loading: true })
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+  
+    this.setState({ user: res.data, loading: false })
+  }
+
   render () {
 
-    const { users, loading } = this.state
+    const { users, user, loading } = this.state
 
     return (
       <Router>
@@ -68,6 +79,9 @@ class App extends Component {
               </Fragment>
             )} />
             <Route exact path='/about' component={About} />
+            <Route exact path='/users/:login' render={props => (
+              <User {...props} getUser={this.getUser} user={user} loading={loading} />
+            )} />
           </Switch>
         </div>
       </div>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types' 
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+const UserItem = ({ user: { avatar_url, login } }) => {
     return (
         <div className='card text-center'>
             <img src={avatar_url} alt='' className='round-img' style={{ width: '60px'}}/>
             <h3>{login}</h3>
-            <a href={html_url} className='btn btn-dark btn-sm my-1'>More</a>
+            <Link to={`users/${login}`} className='btn btn-dark btn-sm my-1'>More</Link>
         </div>
     )
 }
