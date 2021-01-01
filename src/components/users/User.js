85530335c338ba7dd39780446650d3d722a8ba3e
timosha <tinaatimkovaa@gmail.com>
2021-01-01 16:19:41 -0500

    user profile

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -41,6 +41,37 @@ class User extends Component {
                 </Link>
                 Hireable: {' '}
                 { hireable ? <i className='fas fa-check text-success'/> : <i className='fas fa-times-circle text-danger'/> }
+                <div className='card grid-2'>
+                    <div>
+                    <img src={avatar_url} alt='user avatar' className='round-img' styles={{ width: '150px' }}/>
+                    <h1>{name}</h1>
+                    <p>Location: {location}</p>
+                    </div>
+                    <div>
+                        {bio && <Fragment>
+                            <h3>Bio:</h3>
+                        <p>{bio}</p>
+                            </Fragment>}
+                        <a href={html_url} className='btn btn-dark my-1'>Github profile</a>
+                        <ul>
+                        <li><strong>Username: </strong>
+                        {login}
+                        </li>
+                        <li><strong>Company: </strong>
+                        {company}
+                        </li>
+                        <li><strong>Website: </strong>
+                        {blog}
+                        </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Followers: {followers}</p>
+                        <p>Following: {following}</p>
+                        <p>Public repos: {public_repos}</p>
+                        <p>Public gists: {public_gists}</p>
+                    </div>
+                </div>
             </Fragment>
         )
     }
