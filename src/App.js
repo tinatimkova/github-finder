08ba6aa087a9d95c5d140f47e37fea8b80b77a6f
timosha <tinaatimkovaa@gmail.com>
2021-01-01 23:01:15 -0500

    finish user page

--- src/App.js
+++ src/App.js
@@ -51,7 +51,10 @@ class App extends Component {
   getUser = async (username) => {
     this.setState({ loading: true })
 
-    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    const res = await axios({
+      method: 'get',
+      url: `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    })
   
     this.setState({ user: res.data, loading: false })
   }
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -42,8 +42,8 @@ class User extends Component {
                 Hireable: {' '}
                 { hireable ? <i className='fas fa-check text-success'/> : <i className='fas fa-times-circle text-danger'/> }
                 <div className='card grid-2'>
-                    <div>
-                    <img src={avatar_url} alt='user avatar' className='round-img' styles={{ width: '150px' }}/>
+                    <div className='all-center'>
+                    <img src={avatar_url} alt='user avatar' className='round-img' style={{ width: '150px' }} />
                     <h1>{name}</h1>
                     <p>Location: {location}</p>
                     </div>
@@ -52,25 +52,33 @@ class User extends Component {
                             <h3>Bio:</h3>
                         <p>{bio}</p>
                             </Fragment>}
-                        <a href={html_url} className='btn btn-dark my-1'>Github profile</a>
+                        <a href={html_url} className='btn btn-dark my-1'>Visit Github Profile</a>
                         <ul>
-                        <li><strong>Username: </strong>
-                        {login}
+                        <li>
+                            {login && <Fragment>
+                                <strong>Username: </strong>
+                                    {login}
+                                </Fragment>}
                         </li>
-                        <li><strong>Company: </strong>
-                        {company}
+                        <li>
+                            {company && <Fragment>
+                                <strong>Company: </strong>
+                                    {company}
+                                </Fragment>}
                         </li>
-                        <li><strong>Website: </strong>
-                        {blog}
+                        <li>{blog && <Fragment>
+                                <strong>Website: </strong>
+                                    {blog}
+                                </Fragment>}
                         </li>
                         </ul>
                     </div>
-                    <div>
-                        <p>Followers: {followers}</p>
-                        <p>Following: {following}</p>
-                        <p>Public repos: {public_repos}</p>
-                        <p>Public gists: {public_gists}</p>
-                    </div>
+                </div>
+                <div className='card text-center'>
+                    <div className='badge badge-primary'>Followers: {followers}</div>
+                    <div className='badge badge-success'>Following: {following}</div>
+                    <div className='badge badge-light'>Public Repos: {public_repos}</div>
+                    <div className='badge badge-dark'>Public Gists: {public_gists}</div>
                 </div>
             </Fragment>
         )
